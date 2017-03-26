32947175f7f404fea597ff751b6c6b09b0371d60
Sofia Cerone <scerone17@sbry1617075.attlocal.net>
2017-03-26 16:05:25 -0700

    javascript scroll

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-console.log("what is up");
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+function scrollToSection(event){
+  var section=document.getElementById(event.target.getAttribute("data-section"))
+  var targetY=section.getBoundingClientRect().top+window.pageYOffset
+  window.scroll(0,targetY)
 }
+var links=Array.from(document.querySelectorAll("nav .scroller"))
+links.forEach(function(element,index){
+    element.addEventListener("click",scrollToSection)
+})
